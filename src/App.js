f7a2f2d6aa8cabaa3e1c34c017ca4a9bdbe2f5f0
Samir-ZD <samir.zahreddine@outlook.com>
2023-11-26 02:41:33 +0200

    Configured react-router-dom and created basic layouts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-xl'>Hello World</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar title={"Github Profile Fetcher"}/>
+        <div className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/*' element={<NotFound/>}/>
+          </Routes>
+        </div>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <footer className='footer p-5 text-primary-content footer-center'>
+    <p className='text-lg'>@Samir-ZD</p>
+   </footer>
+  )
+}
+
+export default Footer
