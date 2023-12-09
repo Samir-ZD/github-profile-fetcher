32f4c24619060cc7e7c3172d3ff0f3afeb1532b0
Samir-ZD <samir.zahreddine@outlook.com>
2023-12-09 03:03:10 +0200

    minor ui updates in Footer.jsx & About.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
    <footer className='footer p-5 text-info-content footer-center'>
-    <p className='text-2xl'>@Samir-ZD</p>
+    <span className='text-2xl'><a href='https://github.com/Samir-ZD' target="_blank">@Samir-ZD</a></span>
    </footer>
   )
 }
